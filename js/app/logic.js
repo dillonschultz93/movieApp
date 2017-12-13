753f07d46c23b66c0f444b3baa50c09dafe76606
gabe <grchavez@email.arizona.edu>
2017-12-12 18:32:19 -0700

    Commit

--- js/app/logic.js
+++ js/app/logic.js
@@ -1,11 +1,11 @@
 var movies = ["Super Bad"];
+var moviesToWatch = [];
 
 $("#find-movie").on("click", function(event) {
 
 	event.preventDefault();
 
 	var movie = $("#movie-input").val();
-	console.log(movie);
 	var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=dd113167";
 
 		$.ajax({
@@ -13,12 +13,13 @@ $("#find-movie").on("click", function(event) {
 			method: "GET"
 		}).done(function(response){
 			$("#movie-view").text(JSON.stringify(response));
-			console.log(response.Actors);
-
-
+			console.log(response);
+			console.log(response.Title);
+			console.log(response.Released);
+			console.log(response.Rated);
+			console.log(response.Runtime);
+			console.log(response.Plot);
+			console.log(response.Poster);
 		})
-
-
-
 
 });
